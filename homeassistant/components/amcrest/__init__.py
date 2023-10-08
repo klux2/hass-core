0cd0291f31fedab5b99edfdbcd0ec274e27fece6
Konrad Lux <kolu23@student.bth.se>
2023-10-08 22:49:55 +0200

    [amcrest] extract service handler to reduce cognitive complexity

--- homeassistant/components/amcrest/__init__.py
+++ homeassistant/components/amcrest/__init__.py
@@ -314,6 +314,51 @@ def _start_event_monitor(
     thread.start()
 
 
+async def _async_extract_from_service(
+    hass: HomeAssistant, call: ServiceCall
+) -> list[str]:
+    def have_permission(user: User | None, entity_id: str) -> bool:
+        return not user or user.permissions.check_entity(entity_id, POLICY_CONTROL)
+
+    if call.context.user_id:
+        user = await hass.auth.async_get_user(call.context.user_id)
+        if user is None:
+            raise UnknownUser(context=call.context)
+    else:
+        user = None
+
+    if call.data.get(ATTR_ENTITY_ID) == ENTITY_MATCH_ALL:
+        # Return all entity_ids user has permission to control.
+        return [
+            entity_id
+            for entity_id in hass.data[DATA_AMCREST][CAMERAS]
+            if have_permission(user, entity_id)
+        ]
+
+    if call.data.get(ATTR_ENTITY_ID) == ENTITY_MATCH_NONE:
+        return []
+
+    call_ids = await async_extract_entity_ids(hass, call)
+    entity_ids = []
+    for entity_id in hass.data[DATA_AMCREST][CAMERAS]:
+        if entity_id not in call_ids:
+            continue
+        if not have_permission(user, entity_id):
+            raise Unauthorized(
+                context=call.context, entity_id=entity_id, permission=POLICY_CONTROL
+            )
+        entity_ids.append(entity_id)
+    return entity_ids
+
+
+async def _async_service_handler(hass: HomeAssistant, call: ServiceCall) -> None:
+    args = []
+    for arg in CAMERA_SERVICES[call.service][2]:
+        args.append(call.data[arg])
+    for entity_id in await _async_extract_from_service(hass, call):
+        async_dispatcher_send(hass, service_signal(call.service, entity_id), *args)
+
+
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Amcrest IP Camera component."""
     hass.data.setdefault(DATA_AMCREST, {DEVICES: {}, CAMERAS: []})
@@ -406,46 +451,9 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     if not hass.data[DATA_AMCREST][DEVICES]:
         return False
 
-    def have_permission(user: User | None, entity_id: str) -> bool:
-        return not user or user.permissions.check_entity(entity_id, POLICY_CONTROL)
-
-    async def async_extract_from_service(call: ServiceCall) -> list[str]:
-        if call.context.user_id:
-            user = await hass.auth.async_get_user(call.context.user_id)
-            if user is None:
-                raise UnknownUser(context=call.context)
-        else:
-            user = None
-
-        if call.data.get(ATTR_ENTITY_ID) == ENTITY_MATCH_ALL:
-            # Return all entity_ids user has permission to control.
-            return [
-                entity_id
-                for entity_id in hass.data[DATA_AMCREST][CAMERAS]
-                if have_permission(user, entity_id)
-            ]
-
-        if call.data.get(ATTR_ENTITY_ID) == ENTITY_MATCH_NONE:
-            return []
-
-        call_ids = await async_extract_entity_ids(hass, call)
-        entity_ids = []
-        for entity_id in hass.data[DATA_AMCREST][CAMERAS]:
-            if entity_id not in call_ids:
-                continue
-            if not have_permission(user, entity_id):
-                raise Unauthorized(
-                    context=call.context, entity_id=entity_id, permission=POLICY_CONTROL
-                )
-            entity_ids.append(entity_id)
-        return entity_ids
 
     async def async_service_handler(call: ServiceCall) -> None:
-        args = []
-        for arg in CAMERA_SERVICES[call.service][2]:
-            args.append(call.data[arg])
-        for entity_id in await async_extract_from_service(call):
-            async_dispatcher_send(hass, service_signal(call.service, entity_id), *args)
+        await _async_service_handler(hass, call)
 
     for service, params in CAMERA_SERVICES.items():
         hass.services.async_register(DOMAIN, service, async_service_handler, params[0])
